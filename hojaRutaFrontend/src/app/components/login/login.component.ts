824072539e76b356ded96d5c5b42b52226049f87
Alex Alba <alejandroalba999@gmail.com>
2020-03-05 14:16:27 -0600

    user comunicado back y front

--- hojaRutaFrontend/src/app/components/login/login.component.ts
+++ hojaRutaFrontend/src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { $ } from 'protractor';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
--- hojaRutaFrontend/src/app/user/user.component.ts
+++ hojaRutaFrontend/src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 import {RestService} from '../rest.service';
 import {ActivatedRoute, Router} from '@angular/router';
-
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
